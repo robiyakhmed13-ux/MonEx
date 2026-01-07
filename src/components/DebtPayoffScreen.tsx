af5379e540092250eddaa0019bdb1410120af563
Rose <robiyakhmed13@gmail.com>
2026-01-07 18:07:01 +0500

    Update DebtPayoffScreen.tsx

--- src/components/DebtPayoffScreen.tsx
+++ src/components/DebtPayoffScreen.tsx
@@ -201,7 +201,8 @@ export const DebtPayoffScreen = memo(() => {
   const totalDebt = debts.reduce((sum, d) => sum + d.remainingAmount, 0);
 
   return (
-    <div className="min-h-screen bg-background pb-32 px-4 pt-4">
+    <div className="screen-container">
+      <div className="px-4 pt-4">
       {/* Header */}
       <header className="flex items-center justify-between mb-6">
         <div className="flex items-center gap-3">
@@ -409,6 +410,7 @@ export const DebtPayoffScreen = memo(() => {
           </section>
         </>
       )}
+      </div>
 
       {/* Add Debt Modal */}
       {showAddModal && (
